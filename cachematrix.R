91151c928c84f2e31625dd95c6b3f9d748394362
cogphn <jason.deyalsingh@gmail.com>
2014-08-12 07:37:37 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function caches all information about the matrix object we plan
+## to process. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        setMatrix <- function(y){
+                i <<- NULL
+                x <<- y
+        }
+        getMatrix <- function() x
+        getInverse <- function() i
+        setInverse <- function(inverse) i <<- inverse
+        list(setMatrix = setMatrix, getMatrix = getMatrix, getInverse=getInverse, setInverse = setInverse)
+                
 }
 
 
-## Write a short comment describing this function
+## uses the MASS library to calculate the inverse of a matrix within the makeCacheMatrix function, once 
+## it has not been previously calculated 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getInverse()
+        
+        if(!is.null(inverse)){ 
+                message("getting cached data")
+                return(inverse)
+        }
+        m <- x$getMatrix()
+        
+        library(MASS)
+        inv <- ginv(m)
+        x$setInverse(inv)
+        detach("package:MASS", unload=T)
+        inv
+        
 }
